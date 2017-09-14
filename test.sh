089bcdb3cbbf9d469d8f9e7e6f7e660ab5c8073d
oracle <oracle@node1.oracle.com>
2017-09-15 02:35:11 +0900

    Added test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+npm install js-yaml fs require config
+
+mkdir output
+
+node g2gml_to_sparql.js examples/donar_mutation.g2g output/test
+
+node client.js output/test_nodes.sql output/nodes.tsv
+node client.js output/test_edges.sql output/edges.tsv
+
