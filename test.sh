2d93af009066a8dbb957fdd58626036e0eb0e6db
matsusho <matsusho070@gmail.com>
2017-12-29 14:45:29 +0900

    fix test.sh

--- test.sh
+++ test.sh
@@ -2,22 +2,10 @@
 
 npm install js-yaml fs config request path
 
-mkdir output
-
-node g2gml_to_sparql.js examples/donor_mutation.g2g output/test
-
 END_POINT=https://integbio.jp/rdf/sparql
 
-node client.js $END_POINT output/test_donor_nodes.sql output/donor_nodes.tsv
-node client.js $END_POINT output/test_mutation_nodes.sql output/mutation_nodes.tsv
-node client.js $END_POINT output/test_edges.sql output/edges.tsv
+node g2gml_mapper.js pgp $END_POINT examples/donar_muation.g2g output/ 
 
-sh gpg_to_pgx.sh icgc
+node gpg_to_pgx.js icgc
 
 cat examples/icgc.pgql | pgx
-
-node gpg_to_neo.js
-
-rm -rf ./graph.db
-
-neo4j-import --nodes output/donor_nodes.tsv.csv,output/mutation_nodes.tsv.csv --relationships output/edges.tsv.csv --into=./graph.db --ignore-missing-nodes --bad-tolerance 3000000
