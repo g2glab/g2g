4f19df91896d603e307afbbb08d87093b974683b
matsusho <matsusho070@gmail.com>
2018-02-15 17:47:49 +0900

    g2gml_mapper -> g2g

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ npm install js-yaml fs config request path
 
 END_POINT=https://integbio.jp/rdf/sparql
 
-node g2gml_mapper.js pgx $END_POINT examples/donor_mutation.g2g output/icgc
+node g2g.js pg examples/donor_mutation.g2g output/icgc $END_POINT 
 
 cat examples/icgc.pgql | pgx
