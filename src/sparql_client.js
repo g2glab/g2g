45851ef7a9008c776210e2706990b930c5883d41
Hirokazu Chiba <hchiba1@gmail.com>
2024-12-25 01:09:34 +0900

    Replace request with axios

--- src/sparql_client.js
+++ src/sparql_client.js
@@ -1,36 +1,43 @@
 exports.query = query;
 
-const request = require('request');
+const axios = require('axios');
 const fs = require('fs');
 const version = require('commander').version(require("../package.json").version)
 
-function query(endpoint, inputQuery, outFile, callback) {
-  let query;
-  if(fs.existsSync(inputQuery)) {
-    query = fs.readFileSync(inputQuery, 'utf8');
-  } else {
-    query = inputQuery;
-  }
-  const options = {
-    uri: endpoint + '?timeout=0', // infinite 
-    form: {query: query},
-    headers:{ 
-      "User-agent": `g2g/mapper${version}`, 
-      "Accept": "text/tab-separated-values"
+async function query(endpoint, inputQuery, outFile, callback) {
+  try {
+    let query;
+    if (fs.existsSync(inputQuery)) {
+      query = fs.readFileSync(inputQuery, 'utf8');
+    } else {
+      query = inputQuery;
     }
-  };
-  request.post(options, function(error, response, body){
-    if (!error && response.statusCode == 200) {
+
+    const options = {
+      url: endpoint + '?timeout=0', // infinite
+      method: 'POST',
+      data: new URLSearchParams({ query }),
+      headers:{
+        "User-agent": `g2g/mapper${version}`,
+        "Accept": "text/tab-separated-values"
+      }
+    };
+
+    const response = await axios(options);
+
+    if (response.status === 200) {
       const maxrows = response.headers['x-sparql-maxrows'];
-      fs.writeFile(outFile, body, 'utf8', function (err) {
-        if (err != null) {
+      fs.writeFile(outFile, response.data, 'utf8', function (err) {
+        if (err) {
           console.log(err);
         }
         callback(maxrows);
       });
     } else {
-      console.log('error: '+ response.statusCode);
-      console.log(body);
+      console.log('error: '+ response.status);
+      console.log(response.data);
     }
-  });
+  } catch (error) {
+    console.log('Request failed:', error.message);
+  }
 }
